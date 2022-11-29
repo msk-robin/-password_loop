4149970be9d14c059e830b9728ee2038f4a9dc6d
robin-msk <robin.musyoki@kibo.school>
2022-11-29 21:09:57 +0300

    modified the password validator to a working state

--- main.py
+++ main.py
@@ -4,27 +4,33 @@ alphabetic_characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 
                                                                                                                           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
                  'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-numbers = [0,1,2,3,4,5,6,7,8,9]
+numbers = ['0','1','2','3','4','5','6','7','8','9']
 
 # Define fixed values here
 password = input ("Create password: ")
-# Write your code here
-while password:
-  if len(password) <8:
-    if password.count(alphabetic_characters) < 3:
-      if password.count(special_characters) < 3:
-        if password.count(numbers) < 2:
-          print("You need at least 2 number digits!")
-          break
-        print("you need at least 3 special characters!")
-        break
-      print("alphabetics must be at least 3!")
-      break
-    print("length must be at least 8 characters!")
-    break
-# if (len(password)<8):
-#     user_password=list(password)
-#     if any(i in user_password for i in alphabetic_characters if )>=3:
 
-#     print("password must be at least 8 char!")
-#     break
+count = 0
+for i in password:
+  if i in  alphabetic_characters:
+    count +=1
+
+count1 = 0
+for i in password:
+  if i in special_characters:
+    count1+=1
+
+count2 = 0
+for i in password:
+  if i in numbers:
+    count2+=1     
+
+if len(password) < 8:
+  print("Enter at least 8 characters!")
+if count < 3:
+  print("Enter at least 3 alphabetics!")
+
+if count1 < 3:
+  print("Enter at least 3 special characters!") 
+
+if count2 <3:
+  print("Enter at least 3 numbers!")
